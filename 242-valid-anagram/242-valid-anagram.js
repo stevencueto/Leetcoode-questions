7d1ab7d679d1fd6c0c6204f3fdb219955a6295ef
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-06 18:23:12 -0400

    Time: 71 ms (98.13%), Space: 42.5 MB (93.13%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -5,9 +5,18 @@
  */
 var isAnagram = function(s, t) {
     if(s.length !== t.length)return false
-    let x = s.split('').sort().join('');
-    let y = t.split('').sort().join('');
+    const sHs = {}
+    const tHs = {}
+    for(let i = 0; i < s.length; i++){
+        sHs[s[i]] =  sHs[s[i]] + 1 || 1
+        tHs[t[i]] =  tHs[t[i]] + 1 || 1
+    }
+    for(const key in sHs){
+        if(sHs[key] !== tHs[key]) return false
+    }
     
-    return y === x ? true  : false
-    
+    return true
 };
+
+//making a hash map and 2 for loops makes a spaces o(N) s and O(1) time
+
