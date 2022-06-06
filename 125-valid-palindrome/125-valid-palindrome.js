716717817375d042823ec74b17e151951dcc0fa5
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-06 18:02:33 -0400

    Time: 104 ms (45.70%), Space: 46.3 MB (46.27%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+     s = s.replace(/[^a-z0-9]/gi, '')
+    console.log(s)
+    let left = 0;
+    let right = s.length - 1
+    while(left < right){
+        if(s[left].toLowerCase() !== s[right].toLowerCase()) return false
+        left++
+        right--
+    }
+    return true
+};
+
+/*make a variable to with no special characters which is a string using regular expressions,
+create a left index variable to keep track of the left pointer
+create a right index variable to keep track of the right index
+create a while loop to se if the pointers are equal, if they are keep looping, if they are not, break out of the while loop and return false
+*/
