c07e368123580aec725af83f8839a8889c052d35
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-09 18:20:42 -0400

    Time: 17 ms (86.02%), Space: 13.4 MB (95.78%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -6,7 +6,7 @@ class Solution(object):
             "}": "{",
             "]": "["
         }
-        
+        # check if we have a closing string, if we have one, check if we have a stack, if we have a stack then we have to check whether or not have a and then with the [-1] operator we get the last element added to the stack list, if so we pop it, else we return negative, because we have no matching string.
         for i in s:
             if i in closing_string:
                 if stack and stack[-1] == closing_string[i]:
