49d92f7dd837deb7ff613f7b3cafc115ae09d478
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-09 21:27:52 -0400

    Time: 54 ms (78.93%), Space: 14.5 MB (9.89%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -11,4 +11,9 @@ class Solution(object):
             diff = target - n
             if diff in prevMap:
                 return [prevMap[diff], i]
-            prevMap[n] = i
+            else:
+                prevMap[n] = i
+            
+            
+            ## make a hash map, store the values once, then check if the difference of the subsctraction equals to the target
+            ##if so, then return the index for them, else store into hash map.
