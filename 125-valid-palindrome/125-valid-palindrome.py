ed79f32d3b09f87928a70a38bde121c6d6bb0ab7
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-10 00:00:11 -0400

    Time: 1608 ms (15.13%), Space: 14.8 MB (46.41%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,25 +1,37 @@
 class Solution(object):
+    # time complexity is O(n), space complexity O(1): no extra memory.
+#     def isPalindrome(self, s):
+#         l = 0
+#         r = len(s) - 1
+#         #make a 2 pointer funcion to loop through, then check if the char is alphanumerical, then is it is, compare, if they match increment left, decrement right, if they dont return False, do this untile l is not overlapping r.
+        
+#         while(l < r):
+#             while l < r and not self.is_alph_num(s[l]):
+#                 l += 1
+#             while r > l and not self.is_alph_num(s[r]):
+#                 r -= 1
+#             if s[l].lower() != s[r].lower():
+#                 return False
+#             l = l + 1
+#             r = r - 1
+        
+#         return True
+        
+#     def is_alph_num(self, c):
+#         return (ord("A") <= ord(c) <= ord("Z") or 
+#                ord("a") <= ord(c) <= ord("z") or 
+#                ord("0") <= ord(c) <= ord("9"))
+#     #this helper function is to make sure 
+    
     def isPalindrome(self, s):
-        l = 0
-        r = len(s) - 1
-        #make a 2 pointer funcion to loop through, then check if the char is alphanumerical, then is it is, compare, if they match increment left, decrement right, if they dont return False, do this untile l is not overlapping r.
+        new_string = ""
         
-        while(l < r):
-            while l < r and not self.is_alph_num(s[l]):
-                l += 1
-            while r > l and not self.is_alph_num(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                print(s[l])
-                print(s[r])
-                return False
-            l = l + 1
-            r = r - 1
+        """
+        in this case, we are using the isalnum() build in function to check wheather is alphanumeric or not,
+        space complexity is O(n), time is O(n) needed a new memory.
+        """
+        for i in s:
+            if i.isalnum():
+                new_string = new_string + i.lower()
         
-        return True
-        
-    def is_alph_num(self, c):
-        return (ord("A") <= ord(c) <= ord("Z") or 
-               ord("a") <= ord(c) <= ord("z") or 
-               ord("0") <= ord(c) <= ord("9"))
-    #this helper function is to make sure 
+        return new_string == new_string[::-1]
