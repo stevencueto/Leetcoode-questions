6af2d78ed3f39b15459200fb9c7a75da0714f2b2
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-09 21:54:48 -0400

    Time: 38 ms (35.20%), Space: 13.3 MB (82.83%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -6,32 +6,35 @@
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
         """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        """
         make a while loop to lopp through the linked list, if  are not empty, the continue, we are going to check whether l1 is < than l2, 
         when we exit our while loop, we have will check if one of them is null, then make the next node, as the node that is left.
         """
-        
         dummy = ListNode()
+        #make a new ListNode
         tail = dummy
+        #make a reference to the node
         
         while l1 and l2:
+            #while we have the 2 nodes, compare values. if the l1 is less then make it the .next value, esle the l2
             if l1.val < l2.val:
                 tail.next = l1
                 l1 = l1.next
             else:
                 tail.next = l2
                 l2 = l2.next
-            
+                
+            #make sure to extend the tail even more for the reference    
             tail = tail.next
-        
+            
+            
+            
+            #if we still have nodes on either one of the list, append it to the tail
         if l1:
             tail.next = l1
         elif l2:
             tail.next = l2
-        
+           
+        #return the dummy reference node.
         return dummy.next
+            
         
