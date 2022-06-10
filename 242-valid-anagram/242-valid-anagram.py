3fa02767ad390265e2cf9b63a7f306df75cae3ac
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-10 11:16:30 -0400

    Time: 82 ms (23.58%), Space: 15.6 MB (6.72%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -7,9 +7,8 @@ class Solution(object):
         """
         if len(s) != len(t):
             return False
-        
+        """
         hs_s, hs_t = {}, {}
-        
         for i in range(len(s)):
             hs_s[s[i]] = 1 + hs_s.get(s[i], 0)
             hs_t[t[i]] = 1 + hs_t.get(t[i], 0)
@@ -17,5 +16,20 @@ class Solution(object):
             if hs_s[j] != hs_t.get(j, 0):
                 return False
         return True
+        
     
     ## making the hashmap to loop through the strings, then store the char frequency, after that we take in the hs's and them we loop through them and compare the key values, if they don't match we return False, if at the end they match we return True time complexity is O(n or s+t cause he va to loop thorugh it all) space complexity is O(n or s + t) casue we need two hs
+    """
+        count = {}
+        
+        for i in range(len(s)):
+            count[s[i]] =  1 + count.get(s[i], 0)
+            count[t[i]] = count.get(t[i], 0) - 1
+        for i in count:
+            if count[i] != 0:
+                return False
+        return True
+        
+        
+    
+    
