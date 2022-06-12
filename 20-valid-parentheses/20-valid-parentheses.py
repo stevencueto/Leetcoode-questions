84c115e5a19096b5b38281c0351e2c7b465a821f
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-12 12:33:41 -0400

    Time: 42 ms (56.22%), Space: 13.9 MB (22.97%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def isValid(self, s):
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closing = {"}": "{", ")":"(", "]": "["}
         stack = []
-        closing_string = { 
-            ")": "(",
-            "}": "{",
-            "]": "["
-        }
-        # check if we have a closing string, if we have one, check if we have a stack, if we have a stack then we have to check whether or not have a and then with the [-1] operator we get the last element added to the stack list, if so we pop it, else we return negative, because we have no matching string.
+        
         for i in s:
-            if i in closing_string:
-                if stack and stack[-1] == closing_string[i]:
+            if i in closing:
+                if stack and stack[-1] == closing[i]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(i)
+            
         return True if not stack else False
