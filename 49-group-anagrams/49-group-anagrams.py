62a13b2677c026ddd0053a763ed0388dfd355f12
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-15 00:48:08 -0400

    Time: 160 ms (40.88%), Space: 19.9 MB (21.62%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -10,3 +10,10 @@ class Solution:
                 
             output[tuple(count)].append(s)
         return output.values()
+    
+    
+    #T O(m * n solution), S O(n)
+    
+    """
+    make a default dict, so we can store the output, then make a for loop to go through all the strings, then another one to loop through each char, then we are going to use the ASCII value of the string to make it into a touple with keys and a list the the strings that match the values of the count for the ASCII value. return the values only as the outpout
+    """
