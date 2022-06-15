773a42cdc8c94b4d975c4081019c2bec9e9c8f5d
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-15 00:23:46 -0400

    Time: 1141 ms (30.72%), Space: 28.6 MB (9.70%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,12 +1,17 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxSum = 0
-        maxSub = nums[0]
+        output = nums[0]
         
         for n in nums:
+            maxSum += n
+            output = max(output, maxSum)
             if maxSum < 0:
                 maxSum = 0
-            maxSum += n
-            maxSub = max(maxSub, maxSum)
-            
-        return maxSub
+                
+        return output
+    
+    
+    """
+    make a prefixed max subarray witrh index 0, the iterate through our list, if they sub array is larger then our max current sum, update it, if max sum is < 0, we waht to restart it's value
+    """
