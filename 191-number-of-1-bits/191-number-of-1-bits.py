0bc48ee79164b83f594d474f61c9e637a7d601c4
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-16 02:46:07 -0400

    Time: 27 ms (97.78%), Space: 13.8 MB (94.59%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,10 +1,20 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        ans = 0
         
-        while n:
-            ans += n % 2
-            n = n >> 1
-        return ans
+    ### more efficient one would be using this trick too only count the ones inside the n:
     
+        ans = 0 
+        while n:
+            n &= (n - 1)
+            ans += 1
+        return ans
+
+
     """this uses bit manipulation with shifting from the bit with the >> 1, we update the value of the n, we only increment n when the remainder is = 1 and we shift the bits, because is more cpu efficient, if we find n = 0, return the output, time complexity is O(1) the max the while lop can run is 32 times, space complexity O(i)"""
+    
+#     ans = 0
+        
+#         while n:
+#             ans += n % 2
+#             n = n >> 1
+#         return ans
