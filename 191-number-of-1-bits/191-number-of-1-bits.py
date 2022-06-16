6632078ef51a7825adc6afed2fded71a80577257
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-16 02:32:26 -0400

    Time: 64 ms (10.91%), Space: 14 MB (7.54%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -7,4 +7,4 @@ class Solution:
             n = n >> 1
         return ans
     
-    """this uses bit manipulation with shifting from the bit with the >> 1, we update the value of the n, we only increment n when the remainder is = 1 and we shift the bits, because is more cpu efficient, if we find n = 0, return the output"""
+    """this uses bit manipulation with shifting from the bit with the >> 1, we update the value of the n, we only increment n when the remainder is = 1 and we shift the bits, because is more cpu efficient, if we find n = 0, return the output, time complexity is O(1) the max the while lop can run is 32 times, space complexity O(i)"""
