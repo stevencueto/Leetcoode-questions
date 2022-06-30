10d771c26550c3f2e91ca76ceeacb531d1eaa55b
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-06-30 01:17:12 -0400

    Time: 97 ms (5.63%), Space: 14 MB (70.54%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,5 +1,5 @@
-from itertools import accumulate
-
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        return list(accumulate(nums))
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        return nums
