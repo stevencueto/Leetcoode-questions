96a5a284e27e57c53d008f9fdc18014ee3d4d09e
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-07-02 14:43:18 -0400

    Time: 66 ms (29.92%), Space: 13.9 MB (78.92%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -1,40 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        make a while loop to lopp through the linked list, if  are not empty, the continue, we are going to check whether l1 is < than l2, 
-        when we exit our while loop, we have will check if one of them is null, then make the next node, as the node that is left.
-        """
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         #make a new ListNode
         tail = dummy
         #make a reference to the node
         
         while l1 and l2:
-            #while we have the 2 nodes, compare values. if the l1 is less then make it the .next value, esle the l2
-            if l1.val < l2.val:
+            if l1.val <= l2.val:
                 tail.next = l1
                 l1 = l1.next
             else:
                 tail.next = l2
                 l2 = l2.next
-                
-            #make sure to extend the tail even more for the reference    
-            tail = tail.next
-            
-            
-            
-            #if we still have nodes on either one of the list, append it to the tail
+            tail = tail.next 
         if l1:
             tail.next = l1
-        elif l2:
+        if l2:
             tail.next = l2
-           
-        #return the dummy reference node.
-        return dummy.next
             
-        
+        return dummy.next
+    
+    
+    
