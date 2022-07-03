3c2dac9fc0086111cb9b11589a980222f8eb0bf1
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-07-03 19:41:18 -0400

    Time: 35 ms (83.77%), Space: 13.9 MB (55.06%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -5,8 +5,25 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        
+        
+        #less efficient solution, with extra memory:
+        
+        queue = [head]
+        
+        while queue[-1].next:
+            queue.append(queue[-1].next)
+        return queue[len(queue) // 2]
+        
+        
+        #Pointer solution, more efficient, no extra memory
         slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         return slow
+    
+    
+    #by the time the fast one gets to the end of the linked list, we are in the middle, so we return the middle.
+    #O(n) O(1)
