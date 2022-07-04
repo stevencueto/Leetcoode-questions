6c33a2ca8ed0c66c86baac2164968b99c799daeb
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-07-03 20:50:05 -0400

    Time: 49 ms (96.45%), Space: 18 MB (9.95%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -5,6 +5,44 @@
 #         self.next = None
 
 class Solution:
+    #optima solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        
+        catch = self.catch(head)
+        
+        if catch is None:
+            return None
+        
+        #make pointers to traverse, then if they meet stop loop, and return meeting point!
+        
+        p1 = head
+        p2 = catch
+        
+        while p1 != p2:
+            p1 = p1.next
+            p2 = p2.next
+        
+        return p1
+    
+    
+    
+    
+    def catch (self, head):
+        if head is None:
+            return None
+        
+        fast = slow = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return slow
+            
+        return None
+    #extra memory approach
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         visited = set()
         
@@ -18,3 +56,8 @@ class Solution:
                 node = node.next
         
         return None
+    
+    #make a set, add the nodes to the set if we find an existing node in the set, then we return the node, else we add it to the set and them update the node,, O(n) O(n) not so efficient approach
+    
+    
+    
