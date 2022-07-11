b0f46962af2f2f08b8428ffdf7c170af0edba79a
Steven Cueto <96673696+stevencueto@users.noreply.github.com>
2022-07-11 18:38:03 -0400

    Time: 81 ms (29.11%), Space: 16.5 MB (44.98%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -2,18 +2,19 @@
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
-#         self.left = left
+#         self.c = left
 #         self.right = right
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         
-        def valid(node, left, right):
+        
+        #travers through the tree, recursively and find start with infinity values to veirfy weather or not the tree is valid, repeat this for every node in the tree.
+        
+        def valid (node, left, right):
             if not node:
                 return True
             if not (node.val < right and node.val > left):
                 return False
             
-            return (valid(node.left, left, node.val) and
-                    valid(node.right, node.val, right))
-        
-        return valid(root, float("-inf"), float("inf"))
+            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
+        return valid(root, float('-inf'), float("inf"))
